a46ead6e38b1d95baa91880daebe1e38af1c5366
jubayer456 <65710197+jubayer456@users.noreply.github.com>
2022-05-23 23:20:34 +0600

    add business summary

---
+++ src/Pages/Home/BussinessSummary.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+const BussinessSummary = () => {
+    return (
+        <div className='p-12'>
+            <h1 className='text-4xl text-center font-bold'>Million Busines Trust Us</h1>
+            <h3 className='text-xl text-center pb-5 '>Try to understand user expectation</h3>
+            <div className='grid grid-cols-1 lg:grid-cols-4 gap-5'>
+                <div class="card w-max-72 bg-base-100 shadow-xl ">
+                    <div class="card-body text-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 mx-auto" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
+                        </svg>
+                        <h3 className='text-3xl font-bold'>120+</h3>
+                        <p className='text-xl'>Clients</p>
+                    </div>
+                </div>
+                <div class="card w-max-72 bg-base-100 shadow-xl">
+                    <div class="card-body text-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 mx-auto" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z" />
+                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z" clip-rule="evenodd" />
+                        </svg>
+                        <h3 className='text-3xl font-bold'>150M</h3>
+                        <p className='text-xl'>Annual revenue</p>
+                    </div>
+                </div>
+                <div class="card w-max-72 bg-base-100 shadow-xl">
+                    <div class="card-body text-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 mx-auto" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M2 5a2 2 0 012-2h7a2 2 0 012 2v4a2 2 0 01-2 2H9l-3 3v-3H4a2 2 0 01-2-2V5z" />
+                            <path d="M15 7v2a4 4 0 01-4 4H9.828l-1.766 1.767c.28.149.599.233.938.233h2l3 3v-3h2a2 2 0 002-2V9a2 2 0 00-2-2h-1z" />
+                        </svg>
+                        <h3 className='text-3xl font-bold'>33K</h3>
+                        <p className='text-xl'>Reviews</p>
+                    </div>
+                </div>
+                <div class="card w-max-72 bg-base-100 shadow-xl">
+                    <div class="card-body text-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 mx-auto" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2h-1.528A6 6 0 004 9.528V4z" />
+                            <path fill-rule="evenodd" d="M8 10a4 4 0 00-3.446 6.032l-1.261 1.26a1 1 0 101.414 1.415l1.261-1.261A4 4 0 108 10zm-2 4a2 2 0 114 0 2 2 0 01-4 0z" clip-rule="evenodd" />
+                        </svg>
+                        <h3 className='text-3xl font-bold'>50+</h3>
+                        <p className='text-xl'>tools</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BussinessSummary;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from './Banner';
+import BussinessSummary from './BussinessSummary';
 import Contact from './Contact';
 import Reviews from './Reviews';
 import Tools from './Tools';
@@ -9,6 +10,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <Tools></Tools>
+            <BussinessSummary></BussinessSummary>
             <Reviews></Reviews>
             <Contact></Contact>
         </div>
