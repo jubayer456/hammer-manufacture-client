16284163dcb6b550e4c44158dc7e65f741ba3260
jubayer456 <65710197+jubayer456@users.noreply.github.com>
2022-05-23 18:40:19 +0600

    add footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './Pages/Home/Home';
 import About from './Pages/About/About'
 import Blog from './Pages/Blog/Blog'
 import Login from './Pages/Login/Login';
+import Footer from './Pages/Shared/Footer';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path='/blog' element={<Blog />}></Route>
         <Route path='/login' element={<Login />}></Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
