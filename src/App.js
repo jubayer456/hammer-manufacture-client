6b49e674d90e8b8dae4dd2e78461e95005793f56
jubayer456 <65710197+jubayer456@users.noreply.github.com>
2022-05-23 16:22:06 +0600

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './Pages/Shared/NavBar';
 
 function App() {
   return (
     <div >
-
+      <NavBar></NavBar>
     </div>
   );
 }
