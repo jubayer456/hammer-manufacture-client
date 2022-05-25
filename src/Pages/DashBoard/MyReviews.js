cd8d22270b5b289ab01dbd3171d09c704e5761f3
jubayer456 <65710197+jubayer456@users.noreply.github.com>
2022-05-25 17:08:33 +0600

    add review

--- src/Pages/DashBoard/MyReviews.js
+++ src/Pages/DashBoard/MyReviews.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const MyReviews = () => {
+    const [user] = useAuthState(auth);
+    const submitReview = (event) => {
+
+    }
     return (
-        <div>
-            <h1 className='text-2xl py-4'>My Reviews</h1>
+        <div className='mx-3 w-96'>
+            <h1 className='text-2xl py-4'>Please give a review</h1>
+            <form onSubmit={submitReview}></form>
+            <div class="form-control w-full max-w-xs">
+                <label class="label">
+                    <span class="label-text">Your Name</span>
+                </label>
+                <input value={user.displayName} type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" required />
+            </div>
+            <div class="form-control w-full max-w-xs">
+                <label class="label">
+                    <span class="label-text">Your Email</span>
+                </label>
+                <input value={user.email} type="email" placeholder="Type here" class="input input-bordered w-full max-w-xs" required />
+            </div>
+            <div class="form-control w-full max-w-xs">
+                <label class="label">
+                    <span class="label-text">Please Give a ratings out of 5</span>
+                </label>
+                <input type="number" placeholder="Type here" class="input input-bordered w-full max-w-xs" required />
+                <label class="label">
+
+                </label>
+            </div>
+            <div class="form-control w-full max-w-xs">
+                <label class="label">
+                    <span class="label-text">Your Review</span>
+                </label>
+                <textarea class="textarea textarea-bordered" placeholder="Review" required></textarea>
+            </div>
+            <input type="submit" value="Submit" className='mx-auto btn w-full max-w-xs my-2' />
         </div>
     );
 };
