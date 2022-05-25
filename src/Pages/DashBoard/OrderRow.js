9036e499f003944551cfc257390e45cf8eedfeea
jubayer456 <65710197+jubayer456@users.noreply.github.com>
2022-05-25 13:59:48 +0600

    add delete

--- src/Pages/DashBoard/OrderRow.js
+++ src/Pages/DashBoard/OrderRow.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
 const OrderRow = ({ order, index, refetch }) => {
-    const { email, toolsName, price, quantity, address, phnNum } = order;
+    const { email, toolsName, price, quantity, address, phnNum, _id } = order;
+    const deleteOrder = id => {
+        fetch(`http://localhost:5000/booking/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'contenet-type': 'applicatioon/json'
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.deletedCount > 0) {
+                    refetch();
+                    toast.success(`${toolsName} succesfully cancell`)
+                }
+            })
+    }
     return (
         <tr>
             <th>{index}</th>
@@ -11,7 +27,7 @@ const OrderRow = ({ order, index, refetch }) => {
             <td>{price}</td>
             <td>{phnNum}</td>
             <td>{address}</td>
-            <td><button className='btn btn-sm btn-error text-base-100'>Cancel</button></td>
+            <td><button onClick={() => deleteOrder(_id)} className='btn btn-sm btn-error text-base-100'>Cancel</button></td>
         </tr>
     );
 };
